c659665f27914cd0ed25b1ffa960d1f67bb13fe4
kibetnathan <kibetnat@gmail.com>
2025-10-20 14:02:28 +0300

    Add layout for dashboard

---
+++ script.js
@@ -0,0 +1,22 @@
+const signIn = document.getElementById('signIn');
+const main = document.getElementById('main');
+const userForm = document.getElementById('userForm');
+
+function onSubmit(event) {
+    event.preventDefault;
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+
+    const user_data = { name, email};
+    localStorage.setItem('savedData', JSON.stringify(user_data));
+}
+
+userForm.addEventListener('submit', onSubmit);
+const user = JSON.parse(localStorage.getItem('savedData'));
+
+if (user != null){
+    signIn.classList.add('hidden');
+    main.classList.remove('hidden');
+    main.classList.add('flex');
+}
