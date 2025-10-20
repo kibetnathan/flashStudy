cb902c61c5eb55dade9f18ebaee6bec14623f810
kibetnathan <kibetnat@gmail.com>
2025-10-20 15:59:51 +0300

    Add functionality to the sidebar

--- script.js
+++ script.js
@@ -8,15 +8,45 @@ function onSubmit(event) {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
 
-    const user_data = { name, email};
+    const user_data = { name, email };
     localStorage.setItem('savedData', JSON.stringify(user_data));
 }
 
 userForm.addEventListener('submit', onSubmit);
 const user = JSON.parse(localStorage.getItem('savedData'));
 
-if (user != null){
+if (user != null) {
     signIn.classList.add('hidden');
     main.classList.remove('hidden');
     main.classList.add('flex');
 }
+
+const flashButton = document.getElementById('flashcardsButton');
+const mainButton = document.getElementById('mainButton');
+const quizzesButton = document.getElementById('quizzesButton');
+const audioButton = document.getElementById('audioButton');
+
+let currentSection = document.getElementById('mainMenu');
+
+function switchView(sectionId) {
+    const newSection = document.getElementById(sectionId);
+
+    if (newSection === currentSection) {
+        return;
+    }
+    
+    currentSection.classList.remove('flex');
+    currentSection.classList.remove('lg:grid');
+    currentSection.classList.add('hidden');
+    
+    newSection.classList.remove('hidden');
+    newSection.classList.add('flex');
+    newSection.classList.add('lg:grid');
+    
+    currentSection = newSection;
+}
+
+mainButton.addEventListener('click', () => switchView('mainMenu'));
+flashButton.addEventListener('click', () => switchView('flashcards'));
+quizzesButton.addEventListener('click', () => switchView('quizzesSection'));
+audioButton.addEventListener('click', () => switchView('audioSection'));
