bebc31a8fb78ced34009728c7d2457e3b65baec1
kibetnathan <kibetnat@gmail.com>
2025-10-21 08:59:13 +0300

    Add flashcards for microbiology

--- script.js
+++ script.js
@@ -26,7 +26,9 @@ const mainButton = document.getElementById('mainButton');
 const quizzesButton = document.getElementById('quizzesButton');
 const audioButton = document.getElementById('audioButton');
 
-let currentSection = document.getElementById('mainMenu');
+let currentSection
+
+window.addEventListener('load', () => currentSection = document.getElementById('mainMenu'));
 
 function switchView(sectionId) {
     const newSection = document.getElementById(sectionId);
@@ -48,5 +50,50 @@ function switchView(sectionId) {
 
 mainButton.addEventListener('click', () => switchView('mainMenu'));
 flashButton.addEventListener('click', () => switchView('flashcards'));
-quizzesButton.addEventListener('click', () => switchView('quizzesSection'));
-audioButton.addEventListener('click', () => switchView('audioSection'));
+// quizzesButton.addEventListener('click', () => switchView('quizzesSection'));
+// audioButton.addEventListener('click', () => switchView('audioSection'));
+
+const subjects = {
+    microbiology: [
+        "What is the smallest particle of an element that can exist independently?",
+        "What are the three fundamental particles of an atom?",
+        "Where is most of the mass of an atom concentrated?",
+        "What are the charges of protons, neutrons, and electrons?",
+        "Which subatomic particle determines the atomic number of an element?",
+        "Why is a neutral atom electrically neutral?",
+        "Who performed the gold foil experiment?",
+        "What particles did Rutherford use in his gold foil experiment?",
+        "What was the main observation in Rutherford’s experiment?",
+        "What did Rutherford conclude about the atom’s structure?",
+        "How did Rutherford describe the motion of electrons?",
+        "What was the major flaw in Rutherford’s atomic model?",
+        "How does the modern atomic model describe electrons?",
+        "What is the size comparison between the nucleus and the atom?",
+        "What does the nucleus contain?"
+
+    ]
+}
+
+let currentIndex = 0;
+let currentSubject = "microbiology";
+
+function loadQuestion() {
+    const question = subjects[currentSubject][currentIndex];
+  document.getElementById("questionDisplay").textContent = question;
+  document.getElementById("progress").textContent = 
+    `${currentIndex + 1} / ${subjects[currentSubject].length}`;
+}
+ function nextQuestion() {
+  if (currentIndex < subjects[currentSubject].length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    alert("You've reached the end!");
+  }
+}
+function resetQuestions() {
+  currentIndex = 0;
+  loadQuestion();
+}
+
+window.onload = loadQuestion;
